dd23c19f4afc6dd8a2fd60d619b954effe57d98f
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2021-06-24 14:51:48 +0200

    python 3.8 imp 0.51.1 shell

--- code/environments/impetuous-shell.nix
+++ code/environments/impetuous-shell.nix
@@ -2,28 +2,37 @@ with (import <nixpkgs> {}).pkgs;
 with lib;
 
 let
-  myPyPkgs = python37Packages.override {
+  myPyPkgs = python38Packages.override {
     overrides = self: super: {
-      pandas-datareader = super.buildPythonPackage rec {
-        pname = "pandas-datareader";
-        version = "0.9.0";
-        doCheck = false;
+      righteuous-fa = super.buildPythonPackage rec {
+        pname = "righteous-fa";
+        version = "1.2.1";
         src = super.fetchPypi {
           inherit pname version;
-          sha256 = "14gq0kx5b0dpvw5x0fw242dapgir52ljxbk7s4ggzfbadbhw3jxj";
-        };
-        buildInputs = with super;
-          [ pandas numpy requests ];
-      };
-      impetuous-gfa = super.buildPythonPackage rec {
-        pname = "impetuous-gfa";
-        version = "0.24.0";
-        src = super.fetchPypi {
-          inherit pname version;
-          sha256 = "0whyv89hvgw6lk27hsrbiny46nnfaazg4gymgrfwnf3c7kixw839";
+          sha256 = "1qrbk8v2bxm8k6knx33vajajs8y2lsn77j4byviy7mh354xwzsc4";
         };
         buildInputs = with super;
           [ pandas numpy statsmodels scikitlearn scipy patsy ];
+      };
+      impetuous-gfa = super.buildPythonPackage rec {
+        pname = "impetuous-gfa";
+        version = "0.51.1";
+        src = super.fetchPypi {
+          inherit pname version;
+          sha256 = "1q8hg1nnsh35r2gwahv820x5rm8c2aqbgzd29vr4jnv0dz211fyk";
+        };
+        buildInputs = with super;
+          [ pandas numpy statsmodels scikitlearn scipy patsy ];
+      };
+      pypi-matplotlib = super.buildPythonPackage rec {
+        pname = "matplotlib";
+        version = "3.3.3";
+        src = super.fetchPypi {
+          inherit pname version;
+          sha256 = "1v5xwk8amb9b8lx383yy0mgkvzbnfh9d7c4arzjykky4frj0rdmi";
+        };
+        buildInputs = with super;
+          [ numpy certifi ];
       };
     };
   };
@@ -34,11 +43,18 @@ stdenv.mkDerivation rec {
   buildInputs = (with myPyPkgs;
     [
       python impetuous-gfa scikitlearn
-      scipy numpy pandas matplotlib bokeh
-      statsmodels networkx ipython pandas-datareader
-      jupyter
+      scipy numpy pandas bokeh numba
+      statsmodels networkx ipython
+      jupyter righteuous-fa
     ]);
+  src = null;  
   shellHook = ''
+      # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+    
     echo "*****************************"
     echo "* WELCOME TO ${toUpper name} SHELL *"
     echo "*****************************"
