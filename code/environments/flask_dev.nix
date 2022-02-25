45996c5cf8a8c67aa4193742edecfd75b2052dbb
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2022-02-25 14:46:58 +0100

    flask yarn graphtastic

---
+++ code/environments/flask_dev.nix
@@ -0,0 +1,47 @@
+with (import <nixpkgs> {}).pkgs;
+with lib;
+
+let
+  myPyPkgs = python3Packages.override {
+    overrides = self: super: {
+      graphtastic = super.buildPythonPackage rec {
+        pname = "graphtastic";
+        version = "0.11.5";
+        src = super.fetchPypi {
+          inherit pname version;
+          sha256 = "0lz4z6r115gs3lqxiz47ly48lzvi02mcrp4al3k71gicjrfwdvjc";
+        };
+        buildInputs = with super;
+          [ pytoml pytest jinja2 ];
+      };
+    };
+  };
+in
+
+stdenv.mkDerivation rec {
+  name = "flask";
+  buildInputs = (with myPyPkgs;
+    [
+      yarn yarn2nix
+      python
+      graphtastic
+      flask
+      flask-caching
+      flask-cors
+      beautifulsoup4
+      requests
+    ]);
+  src = null;  
+  shellHook = ''
+      # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+    
+    echo "*****************************"
+    echo "* WELCOME TO ${toUpper name} SHELL *"
+    echo "*****************************"
+  '';
+
+}
